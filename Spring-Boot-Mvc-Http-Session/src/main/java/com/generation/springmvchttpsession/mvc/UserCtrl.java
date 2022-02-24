148ffa491d1f45fcc8007c80d5a8b551b2b18a00
nxingram <nxingram@users.noreply.github.com>
2022-02-24 20:19:28 +0100

    tutorial base e session updates

--- Spring-Boot-Mvc-Http-Session/src/main/java/com/generation/springmvchttpsession/mvc/UserCtrl.java
+++ Spring-Boot-Mvc-Http-Session/src/main/java/com/generation/springmvchttpsession/mvc/UserCtrl.java
@@ -1,22 +1,18 @@
 package com.generation.springmvchttpsession.mvc;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 
 import com.generation.springmvchttpsession.entity.User;
 
-// 1) Sessione Http lato server
-//nome parametro del metodo login
-
 @Controller
-@SessionAttributes("user") //1
 public class UserCtrl {
-
+	// 1) Sessione Http lato server
+	
 	// 2) N.B:  Model model: serve solo per debuggare 
 	// e vedere quando lo user viene caricato o rimosso dalla sessione
 	// nelle variabili di debug vedrai una chiave: user
@@ -28,19 +24,23 @@ public class UserCtrl {
 	}
 	
 	@PostMapping("/login")
-	public String login(User user, Model model) { //1 //2
+	public String login(User user, Model model, HttpSession session) { //1 //2
 		
 		user.setName(user.getName().toUpperCase());
+		
+		// tiene traccia della sessione attraverso un cookie "jsessionid";
+		// viene recuperato automaticamente con @SessionAttributes("user") negli altri controllers
+		// setto lo user in sessione
+		session.setAttribute("user", user); 
+	
 		return "redirect:/";
 	}
 	
 	@GetMapping("/logout")
-	public String logout(SessionStatus status, Model model) {//2
+	public String logout(HttpSession session, Model model) {//2
 		
-		// 3) cancello la sessione, e con esso l'utente salvato
-		
-		//3
-		status.setComplete();
+		// cancello la sessione, e con esso l'utente salvato
+		session.invalidate();
 		return "redirect:/";
 	}
 	
