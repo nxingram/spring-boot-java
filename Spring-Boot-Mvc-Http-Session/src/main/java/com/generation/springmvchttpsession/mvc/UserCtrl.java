2f34ff529847c8ee6737da6fabe61e9a4eb04420
nxingram <nxingram@users.noreply.github.com>
2022-03-24 18:57:42 +0100

    rimosso import inutilizzato

--- Spring-Boot-Mvc-Http-Session/src/main/java/com/generation/springmvchttpsession/mvc/UserCtrl.java
+++ Spring-Boot-Mvc-Http-Session/src/main/java/com/generation/springmvchttpsession/mvc/UserCtrl.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.generation.springmvchttpsession.entity.User;
