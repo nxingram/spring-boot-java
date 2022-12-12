d215c46e7035a1f58f953823b86c771a1c3f2b48
nxingram <nxingram@users.noreply.github.com>
2022-12-12 12:42:25 +0100

    chiarimenti

--- Spring-Boot-Mvc-Http-Session/src/main/java/com/generation/springmvchttpsession/mvc/IndexCtrl.java
+++ Spring-Boot-Mvc-Http-Session/src/main/java/com/generation/springmvchttpsession/mvc/IndexCtrl.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import com.generation.springmvchttpsession.entity.User;
 
 //1) Sessione Http lato server
+// @SessionAttributes a volte non funziona bene
+// si può usare @Autowired HttpSession session come in UserCtrl  
+// e poi  session.getAttribute("user") 
 
 @Controller
 @SessionAttributes("user") //1
@@ -15,7 +18,7 @@ public class IndexCtrl {
 	
 	
 	@RequestMapping("/")
-	public String base(User user, Model model) {
+	public String base(User user, Model model) { //user non serve se si usa session.getAttribute("user") 
 		// lo user viene inserito AUTOMATICAMENTE nel model per il template
 		// non è necessario aggiungerlo a mano
 		//model.addAttribute(user)); non serve	
